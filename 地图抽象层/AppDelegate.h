e9480f47d070090f1319bac7c5c6fb8806af1402
biao qiao <biao.qiao@pactera.com>
2016-01-13 10:21:42 +0800

    Initial Commit

---
+++ 地图抽象层/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  地图抽象层
+//
+//  Created by qiaobiao on 16/1/13.
+//  Copyright © 2016年 qiaobiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 地图抽象层/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  地图抽象层
+//
+//  Created by qiaobiao on 16/1/13.
+//  Copyright © 2016年 qiaobiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
